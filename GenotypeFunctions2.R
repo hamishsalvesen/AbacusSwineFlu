58c2a7ba78d18d82ae9ebdf5fac907a6ca64674c
hamishsalvy <hamish.salvesen@roslin.ed.ac.uk>
2020-01-13 09:34:25 +1300

    Updated to avoid issue of rejoining alleles dataframes. Just issue of adding unedited animals back with edited and the already resistant animals

--- GenotypeFunctions2.R
+++ GenotypeFunctions2.R
@@ -1,33 +1,53 @@
-edit_genes <- function(animals, edit_prop = 0.0, edit_type, edit_trials, embryo_trials, fail_rate, death_rate, sel_prop) { 
+edit_genes <- function(animals) { 
   # gene editing workflow
   #     0.0. Subset individuals that would have to be edited
   # Check and see if we have recessives to edit.
-  A_ToEdit <- filter(SPFpop, genoA %in% c("AA", "Aa", "aA"))
-  A_Others <- filter (SPFpop , genoA == "aa")
+  A_ToEdit <- filter(SPFpop, genoA %in% c("A/A", "A/a", "a/A"))
+  A_Others <- filter (SPFpop , genoA == "a/a")
   
-  #     0.1. Sort individuals based on merit to identify the top animals to edit
-  A_ToEdit <- arrange(A_ToEdit, desc(merit)) ## selection criteria for pigs to be edited. May need to be applied to males or females
-  #A_ToEdit <- strsplit(A_ToEdit$genoA, 2)
+  ## Change to piglets for function format
+  if (nrow(A_ToEdit) > 1){
+# Split_A_ToEdit <- strsplit(A_ToEdit$genoA, 2) 
+### need to Strspilt, unnest, add seperated columns
   
-  A_ToEdit <- separate(A_ToEdit, genoA, into = c("Allele1", "Allele2"), sep = "", remove = TRUE) %>% mutate(Allele1 = as.character(Allele1), Allele2 = as.character(Allele2))
-  SPFpopEditA1 <- SPFpopEdit  %>% filter(Allele1 == "A") ## need to select at the same time as genes are in the same pig vessel
-  SPFpopEditA2 <- SPFpopEdit  %>% filter(Allele2 == "A") ## need to select at the same time as genes are in the same pig vessel
+A_ToEdit <- separate(A_ToEdit, genoA, into = c("Allele1", "Allele2"), sep = "/", remove = TRUE) %>% mutate(Allele1 = as.character(Allele1), Allele2 = as.character(Allele2))
   
+#     0.1. Sort individuals based on merit to identify the top animals to edit
+A_ToEdit <- arrange(A_ToEdit, desc(merit)) #%>% top_frac(0.5) ## selection criteria for pigs to be edited. May need to be applied to males or females
+  ##filter out top_frac if only certain animals are to be edited? filter sires/dams or piglets born?
+
+###need to have 'if' for only editing A alleles and keping a in the table
+
+A_ToEditA1 <- A_ToEdit  %>% filter(Allele1 == "A") ## need to select at the same time as genes are in the same pig vessel
+A_ToEditA1$Allele2 <- NULL ### individual alleles are edited
+A_ToEditA2 <- A_ToEdit  %>% filter(Allele2 == "A") ## need to select at the same time as genes are in the same pig vessel
+A_ToEditA2$Allele1 <- NULL
+
   ### filter or select pigs eligible for editing ## filter for piglets at -3 then can put in after CreatePiglets
-  
+  ##rbind A_others with A_toEdit tables  
+
   ## Select proportion of piglets for relevant editing efficiency. #Can do separately as alleles are independently edited?
-  SPFpopEditA1 <- sample_frac(SPFpopEdit, 0.7, replace = FALSE)
-  SPFpopEditA2 <- sample_frac(SPFpopEdit, 0.7, replace = FALSE)
+A_ToEditA1 <- sample_frac(A_ToEditA1, 0.7, replace = FALSE)
+A_ToEditA2 <- sample_frac(A_ToEditA2, 0.7, replace = FALSE) ## random selection of alleles to edit. Essentially editing efficiency proxy
+## need to save the other 30% and remerge below  
+
+A_ToEditA1$Allele1 <- tolower(A_ToEditA1$Allele1) ## use pipe for putting through and avoid splitting table?
+A_ToEditA2$Allele2 <- tolower(A_ToEditA2$Allele2) ### apply to and retain in retain in data frame
+### need to merge the column of Allele2 data to allele1
+
+## want to superimpose new alleles list on top of the A_ToEdit table
+## Add new alleles data over the old alleles in the table. Bind others with edited allele data
   
-  SPFpopEditA1$Allele1 <- tolower(SPFpopEditA1$Allele1)
-  SPFpopEditA1$Allele2 <- NULL
-  SPFpopEditA2$Allele2 <- tolower(SPFpopEditA2$Allele2) ### apply to and retain in retain in data frame
-  SPFpopEditA2$Allele1 <- NULL
+## Need join for allele1 and allele2 to be in the same table for appropriate pigs
+### check that the right alleles are merged back to the relevant piglet!! 
+SPFpopEdit <- full_join(A_ToEditA1, A_ToEditA2, by = "ID") 
+## want to join allele2 column to the rest of the table. 
+
+SPFpopEdit <- SPFpopEdit %>% unite(col = genoA, c(Allele1, Allele2), sep = "/", remove = TRUE)
+
+} ## end of if function
   
-  ### need to add the column of Allele2 data to allele1
-  
-  #SPFpopEdit$genoA <- NULL ### removed in separate function
-  SPFpopEdit <- SPFpopEdit %>% unite(col = genoA, c(Allele1, Allele2), sep = "/", remove = TRUE)
+##join for the original population. anti_join?
   
   # For each recessive to be edited:
   #     1. Select the top edit_prop proportion of animals, currently at least 1 animal always will be edited
